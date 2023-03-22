f31e74eeaae40f735eb152168b642fc56d3e140b
shridharuppar <upparshridhar@gmail.com>
2023-03-22 22:57:10 +0530

    added: Welcome To Employe Wage Compution Programs

---
+++ src/com/bridgelabz/employewagecomputationprograms/EmployeWageComputationPrograms.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employewagecomputationprograms;
+
+public class EmployeWageComputationPrograms {
+public static void main(String[] args) {
+	System.out.println("Welcome To Employe Wage Compution Programs");
+}
+}
