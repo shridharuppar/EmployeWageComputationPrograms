7c067fdc5c4627591bf847e6548f747b3060f4ae
shridharuppar <upparshridhar@gmail.com>
2023-03-22 23:46:41 +0530

    added: Check Employe present or absent

--- src/com/bridgelabz/employewagecomputationprograms/EmployeWageComputationPrograms.java
+++ src/com/bridgelabz/employewagecomputationprograms/EmployeWageComputationPrograms.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.employewagecomputationprograms;
 
 public class EmployeWageComputationPrograms {
-public static void main(String[] args) {
-	System.out.println("Welcome To Employe Wage Compution Programs");
-}
+     public static void main(String[] args) {
+	
+	
+    	 int IS_FULL_TIME = 1;
+    	    double empCheck = Math.floor(Math.random() *10) % 2;
+
+    	    /* Here we use Math.random() to generate random nos between o & 1.
+    	     * We use Math.floor() method to get decimal values only in integer number.
+    	     */
+
+    	    if (empCheck == IS_FULL_TIME) 
+    	    {
+    	        System.out.println("Employee is Present");
+    	    } 
+    	    else 
+    	    {
+    	        System.out.println("Employee is Absent");
+    	    }
+    	}
 }
